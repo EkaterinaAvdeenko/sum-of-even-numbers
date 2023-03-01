07f6dc5a9a8e1046bc54a2575ffa6d1212a65806
Avdeenko Ekaterina <avdeenqoeka@gmail.com>
2023-03-01 17:49:17 +0100

    SumOfEvenNumbers_AvdeenkoEkaterina

--- src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
+++ src/main/java/com/epam/rd/autotasks/arrays/SumOfEvenNumbers.java
@@ -9,9 +9,14 @@ public class SumOfEvenNumbers {
     }
 
     public static int sum(int[] array){
-
+        int sum = 0;
+        if (array == null || array.length == 0)
+            return 0;
+        for (int i = 0; i< array.length; ++i) {
+            if (array[i] % 2 == 0)
+                sum+= array[i];
+        }
+        return sum;
         //put your code here
-
-        throw new UnsupportedOperationException();
     }
 }
